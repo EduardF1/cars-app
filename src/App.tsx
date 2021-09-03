840f15fa035cb5914364db6ced54e3330b4df033
EduardF1 <fischer_eduard@yahoo.com>
2021-09-03 15:29:12 +0200

    add configs for tailwind, craco and add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import './App.css';
+import { Homepage } from './app/containers/homepage';
 
+const AppContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`
 function App() {
-  return (
-    <div className={"App"}>Hello World!</div>
-  );
+    return (
+        <Homepage/>
+    );
 }
 
 export default App;
